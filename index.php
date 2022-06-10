653ad75e38d57fd347cfd56dd3d198f5ae733eeb
galihmustopa3006 <107172046+galihmustopa3006@users.noreply.github.com>
2022-06-10 13:01:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+require 'koneksi.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> TUGAS_WEB</title>
+</head>  <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+    />
+<style>
+    body {
+      background-image: url(10875.jpg);
+      background-size: cover;
+      height: 500px;
+    }
+  </style>
+<body>
+    <nav class="navbar navbar-expand-lg bg-transparent shadow">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <h4>Gamus Motor</h4>
+        </a>
+      </div>
+    </nav>
+    
+
+ <table class="table table-bordered border-primary">
+  <thead>
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">Jenis motor</th>
+      <th scope="col">Service</th>
+      <th scope="col">Spertpart</th>
+      <th scope="col">Harga</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
